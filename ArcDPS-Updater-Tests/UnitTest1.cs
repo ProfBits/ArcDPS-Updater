d7ce4b92280cf1ad796217e6436d1371c709a2c0
ProfBits <53108880+ProfBits@users.noreply.github.com>
2020-11-19 15:02:25 +0100

    #3 Added UnitTest Project

---
+++ ArcDPS-Updater-Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArcDPS_Updater_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
